1e7af8806db6442b2260680abebd9298d65c960d
Aaghaaz Khan <aaghaazkhan12@gmail.com>
2026-02-18 20:00:18 +0530

    Added crop image url

--- src/inference.py
+++ src/inference.py
@@ -30,15 +30,34 @@ def run_inference(image_path):
     exist_ok=True
     )
 
-    save_dir = Path(result[0].save_dir)
-    output_image_path = save_dir / Path(image_path).name
-
     r = result[0]
+    confidence_score = 0.0
+    status = False
+    crop_output_image_path = None
+    output_image_path = None
 
-    status = "True"
-    if r.boxes is None:
-        status = "False"
+    if r.boxes is not None and len(r.boxes) > 0:
+        status = True
 
-    confidence_score = float(r.boxes.conf[0])
+        box = r.boxes.xyxy[0]
+        coords = []
 
-    return output_image_path, status, confidence_score
+        for value in box:
+            coords.append(int(value))
+
+        x1, y1, x2, y2 = coords
+
+        img = cv2.imread(image_path)
+        crop_img = img[y1:y2, x1:x2] # img[y1:y2, x1:x2]
+
+        save_dir = Path(result[0].save_dir)
+        crop_save_dir = save_dir/"crop"
+        crop_save_dir.mkdir(exist_ok=True)
+
+        output_image_path = save_dir / Path(image_path).name
+        crop_output_image_path = crop_save_dir / Path(image_path).name
+        cv2.imwrite(crop_output_image_path, crop_img)
+
+        confidence_score = float(r.boxes.conf[0])
+
+    return output_image_path, status, confidence_score, crop_output_image_path
--- src/main.py
+++ src/main.py
@@ -6,6 +6,8 @@ from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import FileResponse
 from src.inference import run_inference # our custom function for tattoo detection
 OUTPUT_DIR = r"C:\Users\Aaghaaz Khan\Personal\NoteActive\AI_Training\runs\detect\src\outputs\predict"
+CROP_OUTPUT_DIR = r"C:\Users\Aaghaaz Khan\Personal\NoteActive\AI_Training\runs\detect\src\outputs\predict\crop"
+
 
 # =========================================
 # App Initialization
@@ -35,13 +37,15 @@ def predict(image: UploadFile = File(..., description="Upload a tattoo image")):
 
     image.file.close()
     
-    output_image_path, status, confidence_score = run_inference(image_path)
+    output_image_path, status, confidence_score, crop_output_image_path = run_inference(image_path)
     filename = os.path.basename(output_image_path)
+    crop_filename = os.path.basename(crop_output_image_path)
 
     return {
         "tattoo_status": status,
         "confidence_score": confidence_score,
-        "output_path": f"http://127.0.0.1:8000/output_file/{filename}"
+        "output_url": f"http://127.0.0.1:8000/output_file/{filename}",
+        "crop_output_url": f"http://127.0.0.1:8000/crop_output_file/{crop_filename}"
     }
 
 #--------------------------------------------------------------
@@ -53,5 +57,14 @@ def output_file(filename: str):
     file_path = os.path.join(OUTPUT_DIR, filename)
     return FileResponse(
         file_path,
+        media_type="image/jpeg"
+    )
+
+@app.get("/crop_output_file/{filename}")
+
+def crop_output_file(filename: str):
+    crop_file_path = os.path.join(CROP_OUTPUT_DIR, filename)
+    return FileResponse(
+        crop_file_path,
         media_type="image/jpeg"
     )
